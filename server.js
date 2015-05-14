9e3c83d7d6c8d0219941ff02e416606f14b81cf5
pfgc9 <aidan.bloore@gmail.com>
2015-05-14 00:07:40 +0000

    Remove comments

--- server.js
+++ server.js
@@ -4,18 +4,6 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var r = require('rethinkdbdash')();
 
-/*
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-app.get('/js.js', function(req, res){
-  res.sendFile(__dirname + '/js.js');
-});
-app.get('/css.css', function(req, res){
-  res.sendFile(__dirname + '/css.css');
-});
-*/
-
 app.use(express.static('www')); 
 
 
