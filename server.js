747bc78be1b4b0ba19254e0ed9facb0c70fc5229
pfgc9 <aidan.bloore@gmail.com>
2015-05-14 00:06:13 +0000

    Compress app.gets

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var r = require('rethinkdbdash')();
 
+/*
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,6 +14,10 @@ app.get('/js.js', function(req, res){
 app.get('/css.css', function(req, res){
   res.sendFile(__dirname + '/css.css');
 });
+*/
+
+app.use(express.static('www')); 
+
 
 http.listen(process.env.PORT, function(){
   console.log('listening on *:3000');
